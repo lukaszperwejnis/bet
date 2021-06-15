40bf2fa69a5a3b8d07c667088de2db6307228fa6
lukaszperwejnis <lukasz.perwejnis@gmail.com>
2021-06-15 20:22:47 +0200

    Test

--- packages/server/lib/services/CronService.ts
+++ packages/server/lib/services/CronService.ts
@@ -1,11 +1,7 @@
 import { CronJob } from "cron";
 
 export class CronService {
-  public getValidationCron(): CronJob {
-    // return new CronJob('1000', function() {
-    //     console.log(new Date());
-    // }, null);
-
+  getValidationCron(): CronJob {
     return new CronJob("*/10 * * * * *", function () {
       console.log(new Date().getSeconds());
     });
