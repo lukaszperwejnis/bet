0cc3fead287487839f0c1e8da4e3e42c7beef944
lukaszperwejnis <lukasz.perwejnis@gmail.com>
2021-06-23 16:14:40 +0200

    feat(app): removed tests

--- packages/app/test/helpers/getRandomToken.js
+++
@@ -1,4 +0,0 @@
-import faker from 'faker';
-export function getRandomToken() {
-    return faker.internet.password(155);
-}
--- packages/app/test/helpers/getRandomToken.ts
+++
@@ -1,5 +0,0 @@
-import faker from 'faker';
-
-export function getRandomToken(): string {
-  return faker.internet.password(155);
-}
