e58c0579232735326a87da1e46298c79dd43f20a
lukaszperwejnis <lukasz.perwejnis@gmail.com>
2021-08-12 16:37:14 +0200

    Fixed lint script

--- scripts/packages/lint.sh
+++ scripts/packages/lint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "┏━━━ 🕵️‍♀️ LINT: eslint src --ext ts,tsx ━━━━━━━"
-yarn eslint src --ext ts,tsx
+echo "┏━━━ 🕵️‍♀️ LINT: eslint src --ext ts,tsx,js,jsx ━━━━━━━"
+yarn eslint src --ext ts,tsx,js,jsx
